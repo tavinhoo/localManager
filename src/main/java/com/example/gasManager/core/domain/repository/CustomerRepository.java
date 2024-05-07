c9127b33ad27a255a25182a9604714253721d113
Otávio <luizotaviomtbs@gmail.com>
2024-05-07 16:43:47 -0300

    Implementação do repository - compreensão

--- src/main/java/com/example/gasManager/core/domain/repository/CustomerRepository.java
+++ src/main/java/com/example/gasManager/core/domain/repository/CustomerRepository.java
@@ -1,4 +1,8 @@
 package com.example.gasManager.core.domain.repository;
+import com.example.gasManager.core.domain.model.Customer;
+
+import java.util.List;
 
 public interface CustomerRepository {
+    List<Customer> findAll();
 }
---
+++ src/main/java/com/example/gasManager/infrastructure/persistence/CustomerRepositoryImpl.java
@@ -0,0 +1,6 @@
+package com.example.gasManager.infrastructure.persistence;
+
+import com.example.gasManager.core.domain.repository.CustomerRepository;
+
+public class CustomerRepositoryImpl implements CustomerRepository {
+}
