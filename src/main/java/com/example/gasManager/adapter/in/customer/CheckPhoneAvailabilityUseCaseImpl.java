4ccc3a1da132e23ce8d2eeda8fbfaa0ea303db7f
Luiz Otávio <luizotaviomtbs@gmail.com>
2024-05-15 17:46:45 -0300

    CheckPhoneAvailabilityUseCaseImpl + GetAllCustomersUseCaseImpl -> Caso de uso do sistema e implementação para funcionalidada da CreateCustomerUseCaseImpl

---
+++ src/main/java/com/example/gasManager/adapter/in/customer/CheckPhoneAvailabilityUseCaseImpl.java
@@ -0,0 +1,26 @@
+package com.example.gasManager.adapter.in.customer;
+
+import com.example.gasManager.application.usecases.customer.CheckPhoneAvailabilityUseCase;
+import com.example.gasManager.core.domain.repository.CustomerRepository;
+import com.example.gasManager.core.domain.model.Customer;
+
+import java.util.List;
+
+public class CheckPhoneAvailabilityUseCaseImpl implements CheckPhoneAvailabilityUseCase {
+
+    private CustomerRepository customerRepository;
+    public CheckPhoneAvailabilityUseCaseImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public Boolean phoneIsAvailable(String phoneNumber) {
+        List<Customer> allCustomers = customerRepository.findAll();
+        for (Customer customer : allCustomers) {
+            if(customer.getPhone_1() == phoneNumber || customer.getPhone_2() == phoneNumber){
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/com/example/gasManager/adapter/in/customer/GetAllUsersUseCaseImpl.java
@@ -0,0 +1,21 @@
+package com.example.gasManager.adapter.in.customer;
+
+import com.example.gasManager.application.usecases.customer.GetAllCustomersUseCase;
+import com.example.gasManager.core.domain.repository.CustomerRepository;
+import com.example.gasManager.core.domain.model.Customer;
+
+import java.util.List;
+
+public class GetAllUsersUseCaseImpl implements GetAllCustomersUseCase {
+
+    private CustomerRepository customerRepository;
+
+    public GetAllUsersUseCaseImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+}
